86e0cc2a8c58021a58b4b10a32f13d419b49d9cf
visala123 <visala.k@algorims.com>
2025-07-03 15:51:58 +0800

    initial commit

--- aws-dynamodb/backend.tf
+++ aws-dynamodb/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-v"
+    bucket = "terraform-remote-state-v"
     key    = "terraform.tfstate.DynamoDB"
     region = "ap-northeast-2"
   }
